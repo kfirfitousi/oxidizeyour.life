86d4d6a4530dff78caca9ef02318603d50e8f40a
kfirfitousi <kfirp84@gmail.com>
2023-03-18 22:54:23 +0200

    fix: add viewport meta tag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -23,6 +23,7 @@ export default function Document() {
           href="/favicon-16x16.png"
         />
         <link rel="manifest" href="/site.webmanifest" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <body>
         <Main />

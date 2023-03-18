cf7488f05ec44fe500bd5d0fd48185e9a64d644e
kfirfitousi <kfirp84@gmail.com>
2023-03-18 15:12:30 +0200

    fix: better regex

--- src/components/rewrite-card.tsx
+++ src/components/rewrite-card.tsx
@@ -19,6 +19,8 @@ type RewriteCardProps = {
 };
 
 export function RewriteCard({ rewrite }: RewriteCardProps) {
+  const re = new RegExp(`(${rewrite.of.map((s) => s.name).join(")|(")})`, "gi");
+
   return (
     <div className="w-full p-2 max-xs:min-h-fit sm:aspect-square">
       <div className="flex h-full flex-col gap-1 rounded bg-slate-200 p-4 text-slate-700">
@@ -29,45 +31,50 @@ export function RewriteCard({ rewrite }: RewriteCardProps) {
           {rewrite.name}
         </Link>
         <div className="font-light">
-          {rewrite.description.split(" ").map((word, i) => {
-            const software = rewrite.of.find((s) =>
-              new RegExp(`^['"‘\`\(\[]?${s.name}`, "i").test(word)
-            );
+          {rewrite.description
+            .split(re)
+            .filter(Boolean)
+            .map((word, i) => {
+              const software = rewrite.of.find(
+                (s) => s.name === word.toLowerCase()
+              );
 
-            if (!software) return `${word} `;
+              if (!software) return word;
 
-            return (
-              <HoverCard key={i}>
-                <HoverCardTrigger className="font-normal hover:underline">
-                  {word}
-                </HoverCardTrigger>{" "}
-                <HoverCardContent>
-                  <div className="flex flex-col text-slate-600">
-                    <div className="text-lg font-semibold">{software.name}</div>
-                    <div className="pb-2">{software.description}</div>
-                    {software.url && (
-                      <a
-                        href={software.url}
-                        className="flex gap-1 text-slate-500 hover:underline"
-                      >
-                        <ExternalLink className="h-5 w-5" aria-hidden />
-                        <span>{software.url}</span>
-                      </a>
-                    )}
-                    {software.github && (
-                      <a
-                        href={software.github}
-                        className="flex gap-1 text-slate-500 hover:underline"
-                      >
-                        <Github className="h-5 w-5" aria-hidden />
-                        <span>{software.github}</span>
-                      </a>
-                    )}
-                  </div>
-                </HoverCardContent>
-              </HoverCard>
-            );
-          })}
+              return (
+                <HoverCard key={i}>
+                  <HoverCardTrigger className="font-normal hover:underline">
+                    {word}
+                  </HoverCardTrigger>
+                  <HoverCardContent>
+                    <div className="flex flex-col text-slate-600">
+                      <div className="text-lg font-semibold">
+                        {software.name}
+                      </div>
+                      <div className="pb-2">{software.description}</div>
+                      {software.url && (
+                        <a
+                          href={software.url}
+                          className="flex gap-1 text-slate-500 hover:underline"
+                        >
+                          <ExternalLink className="h-5 w-5" aria-label="URL" />
+                          <span>{software.url}</span>
+                        </a>
+                      )}
+                      {software.github && (
+                        <a
+                          href={software.github}
+                          className="flex gap-1 text-slate-500 hover:underline"
+                        >
+                          <Github className="h-5 w-5" aria-label="GitHub" />
+                          <span>{software.github}</span>
+                        </a>
+                      )}
+                    </div>
+                  </HoverCardContent>
+                </HoverCard>
+              );
+            })}
         </div>
         <div className="mt-auto flex flex-col pt-2 text-sm text-slate-600">
           {rewrite.url && (
